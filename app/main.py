9213535b9c92d3e16a0ea6dce1bc2262e916a727
Andre Marcio <andre.marcio.ti@gmail.com>
2026-05-01 17:04:26 -0300

    verify task

--- app/main.py
+++ app/main.py
@@ -1,9 +1,26 @@
-from datetime import datetime  # DO NOT CHANGE THIS IMPORT
+from datetime import datetime, time
 
 
 def main():
-    # write your code here
-    pass
+    print("Starting log generator... (Press Ctrl+C to stop)")
+
+    try:
+        while True:
+            now = datetime.now()
+
+            name_file = now.strftime("app-%H_%M_%S.log")
+
+            content = now.strftime("%Y-%m-%d %H:%M:%S")
+
+            with open(name_file, "w") as file:
+                file.write(content)
+
+            print(f"{content} {name_file}")
+
+            time.sleep(1)
+
+    except KeyboardInterrupt:
+        print(f"Aplication terminated by user.")
 
 
 if __name__ == "__main__":
